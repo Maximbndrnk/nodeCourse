d9d5b7b9dfe24a4841cc3b8aaa9ab9e4d94609ee
Maximbndrnk <maximbndrnk@gmail.com>
2018-08-31 11:07:28 +0300

    final book parser

--- parser/app.js
+++ parser/app.js
@@ -2,16 +2,18 @@ const cheerio = require('cheerio');
 const needle = require('needle');
 const fs = require('fs');
 
-let i =1;
+let i = 1;
 let mainLink = 'http://loveread.ec/read_book.php?id=54931&p=';
 
-function writeFile (fileName, data) {
+function writeFile(fileName, data) {
     return new Promise((resolve, reject) => {
-        fs.appendFile(fileName, data, function(err) {
-                if(err) {
-                   console.log('unable to write file');
-                }
-            });
+        fs.appendFile(fileName, data, function (err) {
+            if (err) {
+                console.log('unable to write file');
+                reject();
+            }
+        });
+        resolve(true);
     });
 }
 
@@ -21,23 +23,59 @@ function getCategoryLinks(link) {
             if (!error && response.statusCode == 200) {
                 let $ = cheerio.load(response.body);
                 const bookTextArr = $('p.MsoNormal, div.take_h1');
-
-                // console.log('TYPEOF', typeof bookTextArr);
-                // console.log(bookTextArr.text());
-                // bookTextArr.children().html();
-                i++;
-                writeFile(i+'.txt', bookTextArr.text())
                 resolve(bookTextArr.text());
             }
         });
     });
 }
 
-getCategoryLinks(mainLink+'1')
-.then(getCategoryLinks(mainLink+'2'))
-.then(getCategoryLinks(mainLink+'3'))
-.then(getCategoryLinks(mainLink+'4'))
-.then(getCategoryLinks(mainLink+'5'));
+let a = [];
+for (let i = 1, len = 45; i < len; i++) {
+    a.push(getCategoryLinks(mainLink + i + ''));
+}
+
+
+
+Promise.all(a).then(val => {
+    res = '';
+    val.forEach((e) => res += e);
+    writeFile('2.txt', res);
+    
+});
+
+// getCategoryLinks(mainLink + '41')
+//     .then((resp) => {
+//         writeFile('1.txt', resp)
+//             .then(() => {
+//                 getCategoryLinks(mainLink + '42')
+//                     .then((resp) => {
+//                         writeFile('1.txt', resp)
+//                             .then(() => {
+//                                 getCategoryLinks(mainLink + '43')
+//                                     .then((resp) => {
+//                                         writeFile('1.txt', resp)
+//                                             .then(() => {
+//                                                 getCategoryLinks(mainLink + '44')
+//                                                     .then((resp) => {
+//                                                         writeFile('1.txt', resp)
+//                                                             .then(() => {
+//                                                                 // getCategoryLinks(mainLink + '40')
+//                                                                 //     .then((resp) => {
+//                                                                 //         writeFile('1.txt', resp)
+//                                                                 //             .then(() => {
+
+//                                                                 //             });
+//                                                                 //     });
+//                                                             });
+//                                                     });
+//                                             });
+//                                     });
+//                             });
+//                     });
+//             });
+//     });
+
+
 
 
 
